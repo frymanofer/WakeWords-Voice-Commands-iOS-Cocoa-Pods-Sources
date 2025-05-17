2c1a0711ae96cc7b706a881dfa4f1e951ee6f4cc
Ofer Fryman <ofer@getecho.io>
2025-05-17 22:09:02 +0300

    v 52

--- ios/KeyWordRNBridge.h
+++ ios/KeyWordRNBridge.h
@@ -1,6 +1,12 @@
 #import <Foundation/Foundation.h>
-#import <React/RCTEventEmitter.h>
-#import <React/RCTBridge.h>
+#if __has_include(<React/RCTEventEmitter.h>)
+  #import <React/RCTEventEmitter.h>
+  #import <React/RCTBridge.h>
+#else
+  /* React-Core was built as a framework → module name is React_Core */
+  #import <React_Core/RCTEventEmitter.h>
+  #import <React_Core/RCTBridge.h>
+#endif
 
 @interface KeyWordRNBridge : RCTEventEmitter <KeywordDetectionRNDelegate>
 + (void)sendEventWithName:(NSString *)name body:(id)body;
